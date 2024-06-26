a0029ffe7d6fa723ca99e6c9dc55ff38540a4956
danielgao20 <dygao@usc.edu>
2024-06-25 23:39:26 -0700

    temp site

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
 import './style.css'; // Import global styles
 import './mediaqueries.css';  // Responsive styles
-import Header from './components/Header';
-import Profile from './components/Profile';
-import About from './components/About';
-import Experience from './components/Experience';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Profile />
-      <About />
-      <Experience />
-      <Projects />
-      <Contact />
-      <Footer />
+      <div className="coming-soon">
+        <h1>Making updates, new site coming soon :)</h1>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+// import React from 'react';
+// import './style.css'; // Import global styles
+// import './mediaqueries.css';  // Responsive styles
+// import Header from './components/Header';
+// import Profile from './components/Profile';
+// import About from './components/About';
+// import Experience from './components/Experience';
+// import Projects from './components/Projects';
+// import Contact from './components/Contact';
+// import Footer from './components/Footer';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header />
+//       <Profile />
+//       <About />
+//       <Experience />
+//       <Projects />
+//       <Contact />
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default App;
